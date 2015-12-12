59dbda3af79322c672fb34e862c0b54dc5a6b3fe
tzDev <chase@wreet.co>
2015-12-12 13:53:00 -0700

    things the stuff

--- app/controllers/shoppe/products_controller.rb
+++ app/controllers/shoppe/products_controller.rb
@@ -52,7 +52,7 @@ module Shoppe
 
     def safe_params
       file_params = [:file, :parent_id, :role, :parent_type, :file => []]
-      params[:product].permit(:make, :model, :year, :miles, :engine, :transmission, :mpg, :color, :fuel_type, :trim, :sku, :permalink, :description, :short_description, :weight, :price, :cost_price, :tax_rate_id, :stock_control, :active, :featured, :in_the_box, :attachments => [:default_image => file_params, :data_sheet => file_params, :extra => file_params], :product_attributes_array => [:key, :value, :searchable, :public], :product_category_ids => [])
+      params[:product].permit(:make, :model, :year, :miles, :engine, :transmission, :mpg_city, :mpg_highway, :drivetrain, :interior, :color, :fuel_type, :trim, :sku, :permalink, :description, :short_description, :weight, :price, :cost_price, :tax_rate_id, :stock_control, :active, :featured, :in_the_box, :attachments => [:default_image => file_params, :data_sheet => file_params, :extra => file_params], :product_attributes_array => [:key, :value, :searchable, :public], :product_category_ids => [])
     end
 
   end #wreet
