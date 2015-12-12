3c2a2d130a5d7b31773f224419643a982fe78985
tzDev <chase@wreet.co>
2015-12-11 18:53:59 -0700

    stuff

--- lib/shoppe/default_navigation.rb
+++ lib/shoppe/default_navigation.rb
@@ -9,13 +9,13 @@ require 'shoppe/navigation_manager'
 # This is the default navigation manager for the admin interface. 
 #
 Shoppe::NavigationManager.build(:admin_primary) do
-  add_item :customers
-  add_item :orders
+  #add_item :customers
+  #add_item :orders
   add_item :products
   add_item :product_categories
-  add_item :delivery_services
-  add_item :tax_rates
-  add_item :users
-  add_item :countries
+  #add_item :delivery_services
+  #add_item :tax_rates
+  #add_item :users
+  #add_item :countries
   add_item :settings
 end
