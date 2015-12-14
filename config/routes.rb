fb471dfeca4993018046551191964cd635da5c15
tzDev <chase@wreet.co>
2015-12-14 09:30:07 -0700

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -49,5 +49,5 @@ Shoppe::Engine.routes.draw do
   match 'login/reset' => 'sessions#reset', :via => [:get, :post]
 
   delete 'logout' => 'sessions#destroy'
-  root :to => 'dashboard#home'
+  root :to => 'products#index'
 end
